32ebca7dac900ea86ff70a083b98eae88214ab76
gaborsimonits <gaborsimonits@gmail.com>
2022-01-24 11:14:09 +0100

    push dist and src folders

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// compiling tsc to js 
+// terminal command: tsc index / tsc filename
+// watch a file, start watch mode
+// tsc --watch index
+let id = 5;
+console.log('ID:', id);
+// setup config file 
+// tsc --init
+// setup config file
+// tsc --init
+// changing target in config.json to es6 then run tsc, will change var to let in js file
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// compiling tsc to js 
+// terminal command: tsc index / tsc filename
+
+// watch a file, start watch mode
+// tsc --watch index
+
+let id: number = 5 
+
+console.log('ID:', id )
+
+// setup config file 
+// tsc --init
+
+// setup config file
+// tsc --init
+
+// changing target in config.json to es6 then run tsc, will change var to let in js file
